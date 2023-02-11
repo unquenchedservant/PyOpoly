9da87a9dcbe589406c3d1aace8415ccf281fd614
Jonathan Thorne <contact@unquenched.tech>
2023-02-11 15:18:54 -0500

    created a board file

--- displays/board.py
+++ displays/board.py
@@ -1,3 +1,11 @@
 '''
 This will be the main overall screen, but since i want the main menu to be within the board, this will work.
 '''
+import curses, sys
+from utilities import variables as var
+
+def board(stdscr):
+    # I don't need an x or y position for the cursor here, as this is the everything static on the board
+    k = 0 # K holds any input. 
+
+    
